df493082ae5b147bd5b7c7681b088169715461f7
Dimdim28 <wwwdimanes1@gmail.com>
2021-10-20 11:46:11 +0300

    all is ready

--- Exercises/1-callback.js
+++ Exercises/1-callback.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const iterate = (obj, callback) => null;
+const iterate = (obj, callback) => {
+    const allKeys = Object.keys(obj);
+    for( const key of allKeys){
+        const value = obj[key];
+        callback(key,value,obj);
+    }
+}
+
 
 module.exports = { iterate };
